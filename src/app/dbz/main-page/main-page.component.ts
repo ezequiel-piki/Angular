fb0e12690d02ff243041f02d1b2149d8d8ff34f6
ezequiel-piki <ezequielpiki23@gmail.com>
2022-04-27 17:59:40 -0300

    avanzando en el curso

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -3,6 +3,7 @@ import { Personaje } from '../interfaces/dbz.interface';
 
 
 
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
@@ -19,23 +20,5 @@ export class MainPageComponent {
      nombre:'Vegeta',
      poder :8500
    }
-];
-
-  nuevo:Personaje = {
-    nombre: '',
-    poder : 0 
-  };
-
-  
-  agregar (){
-    if(this.nuevo.nombre.trim().length===0){
-      return;
-    }
-    this.personajes.push(this.nuevo)
-    this.nuevo = {
-      nombre:'',
-      poder: 0
-    }
-    
-  };
+  ];
 }
